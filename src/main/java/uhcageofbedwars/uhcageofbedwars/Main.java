ad528037d96701a6b81db4bd855e944bcdb1f7b8
tkorsm <andrewlecl01@gmail.com>
2022-08-24 15:58:08 -0400

    init

---
+++ src/main/java/uhcageofbedwars/uhcageofbedwars/Main.java
@@ -0,0 +1,18 @@
+package uhcageofbedwars.uhcageofbedwars;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        Bukkit.getLogger().info("Hello world! 1");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
